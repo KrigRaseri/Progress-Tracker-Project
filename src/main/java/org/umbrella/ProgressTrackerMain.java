6f803f0f2b27c84ec6c356cf6bb205ae61947c08
Krig Raseri <krigraseri@gmail.com>
2023-06-17 06:45:52 -0500

    Update ProgressTrackerMain.java

--- src/main/java/org/umbrella/ProgressTrackerMain.java
+++
@@ -1,7 +0,0 @@
-package org.umbrella;
-
-public class ProgressTrackerMain {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/org/umbrella/tracker/ProgressTrackerMain.java
@@ -0,0 +1,9 @@
+package org.umbrella.tracker;
+
+import org.umbrella.tracker.menu.MainMenu;
+
+public class ProgressTrackerMain {
+    public static void main(String[] args) {
+        MainMenu.runMenu();
+    }
+}
