1c4711af79e01fb83050039ff8d9e3b78ab43be5
Krig Raseri <krigraseri@gmail.com>
2023-06-22 22:23:02 -0500

    Update ProgressTrackerMain.java

--- src/main/java/org/umbrella/tracker/ProgressTrackerMain.java
+++ src/main/java/org/umbrella/tracker/ProgressTrackerMain.java
@@ -1,9 +1,14 @@
 package org.umbrella.tracker;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import org.umbrella.tracker.menu.MainMenu;
 
 public class ProgressTrackerMain {
     public static void main(String[] args) {
-        MainMenu.runMenu();
+        Injector injector = Guice.createInjector();
+        MainMenu mainMenu = injector.getInstance(MainMenu.class);
+
+        mainMenu.runMenu();
     }
 }
