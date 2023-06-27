32dfbce7cc9bc43e465a70830bdbdb82ad1938cc
Krig Raseri <krigraseri@gmail.com>
2023-06-26 21:58:48 -0500

    Create StudentTest.java

---
+++ src/test/java/org/umbrella/tracker/student/StudentTest.java
@@ -0,0 +1,59 @@
+package org.umbrella.tracker.student;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.mockito.Mock;
+
+class StudentTest {
+    @Mock
+    Student student;
+
+
+    @BeforeEach
+    void setUp() {
+        student = new Student("John", "Doe", "jonD@goopy.com");
+    }
+
+    @Test
+    void testDisplayStudentInfo_correct() {
+        // Arrange
+        student.setId(61682);
+        student.setJava(90);
+        student.setDsa(85);
+        student.setDatabases(75);
+        student.setSpring(80);
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        PrintStream originalPrintStream = System.out;
+        System.setOut(printStream);
+
+        // Act
+        student.displayStudentInfo();
+        System.out.flush();
+        System.setOut(originalPrintStream);
+
+        // Assert
+        String expectedOutput = "61682 points: Java=90; DSA=85; Databases=75; Spring=80\n";
+        assertThat(outputStream.toString()).isEqualTo(expectedOutput);
+    }
+
+//61682 points: Java=90; DSA=85; Databases=75; Spring=80
+    @Test
+    void testEquals() {
+        Student student2 = new Student("John", "Doe", "jonD@goopy.com");
+        assertThat(student).isEqualTo(student2);
+    }
+
+    @Test
+    void testNotEquals() {
+        Student student2 = new Student("Jane", "Doe", "jonD@goopy.com");
+        assertThat(student).isNotEqualTo(student2);
+    }
+}
+
