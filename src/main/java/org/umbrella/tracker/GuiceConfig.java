25796546dde67ed553cc9e7ec49c4a8c9a36ec6a
Krig Raseri <krigraseri@gmail.com>
2023-07-02 00:47:40 -0500

    Update GuiceConfig.java

--- src/main/java/org/umbrella/tracker/GuiceConfig.java
+++ src/main/java/org/umbrella/tracker/GuiceConfig.java
@@ -1,12 +1,16 @@
 package org.umbrella.tracker;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import org.umbrella.tracker.menu.MainMenu;
 import org.umbrella.tracker.student.StudentService;
 import org.umbrella.tracker.student.StudentServiceImpl;
 
 public class GuiceConfig extends AbstractModule {
     @Override
     protected void configure() {
-        bind(StudentService.class).to(StudentServiceImpl.class);
+        bind(StudentService.class).to(StudentServiceImpl.class).in(Singleton.class);
+        bind((StudentServiceImpl.class)).in(Singleton.class);
+        bind(MainMenu.class).in(Singleton.class);
     }
 }
