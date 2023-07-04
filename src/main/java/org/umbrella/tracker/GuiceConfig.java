6d8ddb18d946a6ea3a2746f37c0011816a22b38b
Krig Raseri <krigraseri@gmail.com>
2023-07-04 07:17:59 -0500

    Update GuiceConfig.java

--- src/main/java/org/umbrella/tracker/GuiceConfig.java
+++ src/main/java/org/umbrella/tracker/GuiceConfig.java
@@ -2,15 +2,23 @@ package org.umbrella.tracker;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.umbrella.tracker.menu.MainMenu;
 import org.umbrella.tracker.student.StudentService;
 import org.umbrella.tracker.student.StudentServiceImpl;
+import org.umbrella.tracker.student.StudentStatistics;
+import org.umbrella.tracker.student.StudentStatisticsImpl;
 
 public class GuiceConfig extends AbstractModule {
     @Override
     protected void configure() {
         bind(StudentService.class).to(StudentServiceImpl.class).in(Singleton.class);
+        bind(StudentStatistics.class).to(StudentStatisticsImpl.class).in(Singleton.class);
         bind((StudentServiceImpl.class)).in(Singleton.class);
         bind(MainMenu.class).in(Singleton.class);
+
+        Logger logger = LogManager.getLogger("MyLogger");
+        bind(Logger.class).toInstance(logger);
     }
 }
