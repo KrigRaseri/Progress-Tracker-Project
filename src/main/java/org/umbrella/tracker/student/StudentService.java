78bf2a264343a8d6e57794d4e7eba791aa033236
Krig Raseri <krigraseri@gmail.com>
2023-07-04 07:19:10 -0500

    Update StudentService.java

--- src/main/java/org/umbrella/tracker/student/StudentService.java
+++ src/main/java/org/umbrella/tracker/student/StudentService.java
@@ -1,10 +1,13 @@
 package org.umbrella.tracker.student;
 
 import java.util.List;
+import java.util.Map;
 
 public interface StudentService {
     String addPoints(List<String> inputSections);
-    void listStudents();
     void findStudent(int id);
     void addStudents(String[] studentInput);
+    boolean isIdPresent(String id);
+    void listStudents();
+    Map<Integer, Student> getStudentMapCopy();
 }
